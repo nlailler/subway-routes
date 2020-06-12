09045691547e0068bb9128c6bf4db278754fdaea
Nicholas Lailler <nicholas@lailler.org>
2020-06-12 18:06:32 -0400

    add button styling, add alert if no route selected

--- src/DisplayContainer.jsx
+++ src/DisplayContainer.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect } from 'react';
 import { Box, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import { DisplayContext } from './context/DisplayProvider';
 import useActions from './context/useActions';
 import { LOADING_TEXT } from './utils/constants';
@@ -8,8 +9,16 @@ import RoutesContainer from './RoutesContainer';
 import StopsContainer from './StopsContainer';
 import getStops from './utils/getStops';
 
+const useStyles = makeStyles({
+  centerButton: {
+    margin: '2rem',
+    maxHeight: '2rem',
+  },
+});
+
 export default function DisplayContainer() {
   const { routesLoaded, stopsLoaded } = useActions();
+  const styles = useStyles();
 
   useEffect(() => {
     (async () => {
@@ -21,6 +30,11 @@ export default function DisplayContainer() {
 
   const { isLoading, selectedRouteId, stops } = useContext(DisplayContext);
   const onClick = async () => {
+    if(!selectedRouteId) {
+      // TODO: Convert this into a toast or something similar.
+      window.alert('Must select a route');
+      return;
+    }
     stopsLoaded({ stops: await getStops({ routeId: selectedRouteId }) });
   };
   return (
@@ -30,7 +44,7 @@ export default function DisplayContainer() {
         : (
           <Box display="flex" flexDirection="row" alignItems="center">
             <RoutesContainer />
-            <Button variant="outlined" onClick={onClick} style={{ maxHeight: '30px' }}> Get Stops</Button>
+            <Button className={styles.centerButton} variant="outlined" onClick={onClick}> Get Stops</Button>
             <StopsContainer stops={stops}/>
           </Box>
         )
